6cf870944e0795c8a230787afcc3a8ff091dc98d
Вадим <you@example.com>
2021-03-13 19:06:06 +0900

    i go to home

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function initMap() {
 	const uluru = { lat: 62.6705236, lng: 131.1628421 };
 	const map = new google.maps.Map(document.getElementById("map"), {
-		zoom: 4,
+		zoom: 15,
 	center: uluru,
 	});
 	const marker = new google.maps.Marker({
